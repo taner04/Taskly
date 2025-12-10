b783c00f3a78afa465887edb424bf96eae1c2686
taner04 <tanergadomski04@gmail.com>
2025-12-10 12:03:41 +0100

    refactor: simplify user authentication handling in CurrentUserService

--- src/Api/Features/Users/Services/CurrentUserService.cs
+++ src/Api/Features/Users/Services/CurrentUserService.cs
@@ -9,20 +9,10 @@ public sealed class CurrentUserService(IHttpContextAccessor httpContextAccessor)
         ?? throw new InvalidOperationException("HTTP context is not available.");
     
     private ClaimsPrincipal User => HttpContext.User;
-    
-    public ClaimsPrincipal GetCurrentUser()
-    {
-        return User.Identity?.IsAuthenticated != true
-            ? throw new UnauthorizedAccessException("User is not authenticated.")
-            : User;
-    }
 
     public string GetAuth0Id()
     {
-        var userId = 
-            GetCurrentUser().FindFirst(ClaimTypes.NameIdentifier)?.Value ?? throw new UnauthorizedAccessException("user id is missing.");
-        
-        return userId;
+        return GetClaimValue<string>(ClaimTypes.NameIdentifier);
     }
     
     public UserId GetUserId()
@@ -40,7 +30,7 @@ public sealed class CurrentUserService(IHttpContextAccessor httpContextAccessor)
     public T GetClaimValue<T>(string claimType)
     {
         var claimValue = 
-            GetCurrentUser().FindFirst(claimType)?.Value ?? throw new UnauthorizedAccessException($"Claim '{claimType}' is missing.");
+            User.FindFirst(claimType)?.Value ?? throw new UnauthorizedAccessException($"Claim '{claimType}' is missing.");
 
         return (T)Convert.ChangeType(claimValue, typeof(T));
     }
